d63677f9657b76d5e5dc98c52aeeedbceda4398d
Nicholas Durai <nrd5379@psu.edu>
2026-04-27 14:36:35 -0400

    Base game completed and running properly..

--- main.py
+++ main.py
@@ -7,34 +7,37 @@ def get_random(low: int = 1, high: int = 100) -> int:
 
 # Function for getting the users input (guess)
 def get_guess() -> int:
-    is_int = False
-    while is_int == False:
-        guess = input("Enter a guess (integer) 1-100")
-        if type(guess) not int:
+    while True:
+        try:
+            guess = int(input("Enter a guess 1-100: "))
+        except ValueError:
             print("Please enter an integer")
+            continue
+        if 1 <= guess <= 100:
+            return guess
+        print("Please enter a number between 1 and 100")
 
 # Function for feedback
 def feedback(guess: int, target: int) -> str:
     if guess < target:
-        print("You're lower")
+        return "Too low"
     elif guess > target:
-        print("You're higher")
-    elif guess == target:
-        print("Correct!")
+        return "Too high"
+    else:
+        return "Correct!"
 
 # Function to create and play the game
 def play():
     n = get_random()
     attempts = 0
-    correct = False
-    while correct == False:
+    while True:
         attempts += 1
         guess = get_guess()
         print(feedback(guess, n))
-        if feedback(guess, n) == "Correct!":
-            correct = True
+        if guess == n:
+            break
 
     return f"Congratulations! It took you {attempts} tries to guess the number."
 
 if __name__ == "__main__":
-    play()
+    print(play())
